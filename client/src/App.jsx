834659355841092df52a531ee78a49bbad1cca38
Santosh177 <santosh08011999@gmail.com>
2024-08-04 12:17:52 +0530

    add tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function App() {
   return (
-    <div className='text-xl text-slate-200'>
+    <div className='text-xl text-amber-500'>
       <h1>App</h1>
     </div>
   )
