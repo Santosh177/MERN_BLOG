76b0b9f15faaf5f2064c18d4b5f95faf5a2dea7e
Santosh177 <santosh08011999@gmail.com>
2024-09-07 23:06:10 +0530

    create a user schema and signup api

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import authRoutes from './routes/auth.route.js';
+import clc from 'cli-color';
 
 dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8000;
 
 mongoose
   .connect(process.env.MONGO)
@@ -13,8 +18,19 @@ mongoose
     console.log(err);
   });
 
-const app = express();
+app.use(express.json());
 
-app.listen(()=>{
-    console.log("your serve is running on localhost:3000")
-})
+app.get("/", (req, res) => {
+  return res.send({
+    status: 200,
+    message: "Welcome to your blogging app",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(clc.blue(`Server is running at`));
+  console.log(clc.yellow(`http://localhost:${PORT}`));
+});
+
+
+app.use('/api/auth',authRoutes)
