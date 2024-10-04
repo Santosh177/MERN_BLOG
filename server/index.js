edaa41318c94df79898e0effaebc0caa1a22ba2a
Santosh177 <santosh08011999@gmail.com>
2024-10-04 19:39:57 +0530

    add comment section to the post page

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 import authRoutes from './routes/auth.route.js';
 import userRoutes from './routes/user.route.js';
 import postRoutes from './routes/post.route.js';
+import commentRoutes from './routes/comment.route.js';
 import clc from 'cli-color';
 import cookieParser from 'cookie-parser';
 
@@ -39,6 +40,7 @@ app.listen(PORT, () => {
 app.use('/api/auth',authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/post', postRoutes);
+app.use('/api/comment', commentRoutes);
 
 
 app.use((err, req, res, next) => {
